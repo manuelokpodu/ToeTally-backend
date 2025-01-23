7f74c3a551b7fd915e7c661b74890188984ec954
king-Abby <iyiolaabby@gmail.com>
2025-01-23 14:10:47 +0100

    added section-1

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// Setup the entry file
+const express = require("express");
+const connectDB = require("./config/connectDb");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+const app = express();
+
+dotenv.config({ path: ".env" });
+
+connectDB();
+
+app.use(express.json());
+
+const auth = require("./routes/auth");
+
+app.use("/api/v1/auth", auth);
+
+const port = 5000;
+
+const server = app.listen(
+  port,
+  console.log(`Server is running on ${port}`.red.bold)
+);
+
+// iyiolaabby
+// BO05TdtvaXhNviDV
